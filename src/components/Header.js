2fd4dea62a40f1207f7df39c17296ab553f31726
Marcelo Ataíde <msa.trybe@gmail.com>
2022-07-02 10:21:38 -0300

    Requisito 04 - Cria os links de navegação no cabeçalho

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
@@ -28,6 +29,11 @@ class Header extends Component {
         <h2 data-testid="header-user-name">
           { user.name }
         </h2>
+        <nav>
+          <Link data-testid="link-to-search" to="/search">Search</Link>
+          <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+          <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+        </nav>
       </header>
     );
   }
