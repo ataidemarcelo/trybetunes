17a3a32f123da8e079990af23276729616b3eb3e
Marcelo Ataíde <msa.trybe@gmail.com>
2022-07-04 15:22:48 -0300

    Requisito 06 - Adiciona a requisição para pesquisar artistas

---
+++ src/components/CardAlbum.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+class CardAlbum extends Component {
+  render() {
+    const { album } = this.props;
+    return (
+      <div key={ album.collectionName }>
+        <img src={ album.artworkUrl100 } alt="" />
+        <p>{ album.collectionName }</p>
+        <p>{ album.artistName }</p>
+        <Link
+          data-testid={ `link-to-album-${album.collectionId}` }
+          to={ `/album/${album.collectionId}` }
+        >
+          { album.collectionId }
+        </Link>
+      </div>
+    );
+  }
+}
+
+CardAlbum.propTypes = {
+  album: PropTypes.shape({
+    collectionName: PropTypes.string.isRequired,
+    artistName: PropTypes.string.isRequired,
+    artworkUrl100: PropTypes.string.isRequired,
+    collectionId: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default CardAlbum;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+// import Loading from '../components/Loading';
+
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import CardAlbum from '../components/CardAlbum';
 
 class Search extends Component {
   constructor() {
@@ -8,6 +13,10 @@ class Search extends Component {
 
     this.state = {
       artist: '',
+      // isLoading: true,
+      albumList: undefined,
+      showInput: true,
+      artistName: undefined,
     };
   }
 
@@ -15,32 +24,68 @@ class Search extends Component {
     this.setState({ artist: target.value });
   }
 
-  render() {
+  handleClick = async () => {
     const { artist } = this.state;
+    this.setState({
+      showInput: false,
+      artistName: artist,
+    });
+
+    const results = await searchAlbumsAPI(artist);
+    this.setState({
+      albumList: [...results],
+      showInput: true,
+      artist: '',
+    });
+  }
+
+  render() {
+    const { artist, showInput, albumList, artistName } = this.state;
     const minLength = 2;
     const isDisabled = artist.length < minLength;
 
     return (
-      <>
+      <div data-testid="page-search">
         <Header />
-        <div data-testid="page-search">
-          <p>Search</p>
-          <input
-            data-testid="search-artist-input"
-            type="text"
-            placeholder="Digitet o nome do artistta"
-            value={ artist }
-            onChange={ this.handleChange }
-          />
-          <button
-            data-testid="search-artist-button"
-            type="button"
-            disabled={ isDisabled }
-          >
-            Procurar
-          </button>
-        </div>
-      </>
+
+        { showInput
+          ? (
+            <div>
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                placeholder="Digite o nome do artista"
+                value={ artist }
+                onChange={ this.handleChange }
+              />
+              <button
+                data-testid="search-artist-button"
+                type="button"
+                disabled={ isDisabled }
+                onClick={ this.handleClick }
+              >
+                Pesquisar
+              </button>
+            </div>
+          ) : <Loading />}
+
+        { artistName
+          && (
+            <h3>
+              Resultado de álbuns de:
+              {' '}
+              { artistName }
+            </h3>
+          )}
+
+        {albumList && (
+          albumList.length === 0
+            ? 'Nenhum álbum foi encontrado'
+            : albumList.map((album) => (
+              <CardAlbum key={ album.collectionId } album={ album } />
+            ))
+        )}
+      </div>
     );
   }
 }
