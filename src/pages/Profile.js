5819bec7b93010db4eb6be457557e0cdc6641614
Marcelo Ataíde <msa.trybe@gmail.com>
2022-07-10 18:14:03 -0300

    Requisito 13 - Cria a exibição de perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      user: undefined,
+    };
+  }
+
+  async componentDidMount() {
+    const user = await getUser();
+    this.setState({ user });
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <>
         <Header />
-        <div data-testid="page-profile">
-          <p>Profile</p>
-        </div>
+        {!user
+          ? <Loading />
+          : (
+            <div data-testid="page-profile">
+              <p>Profile</p>
+              <p>{ user.name }</p>
+              <p>{ user.email }</p>
+              <p>{ user.description }</p>
+              <img data-testid="profile-image" src={ user.image } alt="" />
+              <Link to="/profile/edit">Editar perfil</Link>
+            </div>
+          )}
       </>
     );
   }
